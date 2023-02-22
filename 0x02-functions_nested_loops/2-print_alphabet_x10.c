7b15c0a8f02c989e971af945473ddae8d63545f1
Mayowa <mayowapopoola110@gmail.com>
2023-02-22 13:35:05 -0800

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /***
- * print_alphabet_x10 - Prints all in lowercase
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
  *
  * Return: on success 1
  */
 void print_alphabet_x10(void)
 {
 	char i, j;
+	
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 1; j <= 10; j++)
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-      }
 }

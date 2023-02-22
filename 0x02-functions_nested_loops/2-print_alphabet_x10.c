3c8a5e63fce950af854bd2c93ec4c43d7545b0c6
Mayowa <mayowapopoola110@gmail.com>
2023-02-22 13:13:44 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/***
+ * print_alphabet_x10 - Prints all in lowercase
+ *
+ * Return: on success 1
+ */
+void print_alphabet_x10(void)
+{
+	char i, j;
+	{
+		for (j = 1; j <= 10; j++)
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+      }
+}

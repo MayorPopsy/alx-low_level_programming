0ea193bcc9b815034197d3f4535570ea6c8b4def
Mayowa <mayowapopoola110@gmail.com>
2023-02-22 12:37:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - A program that prints alphabet in lowercase
+ *
+ * Return: On success 1
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n')
+}

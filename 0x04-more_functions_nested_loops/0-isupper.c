0bf94e7744504cef287288f02719649910caeb14
Mayowa <mayowapopoola110@gmail.com>
2023-02-24 12:49:41 -0800

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ *int _isupper(int c) - A function that checks for uppercase characters 
+ *Return: 1-if c is uppercase or 0-otherwise
+ */
+int _isupper(int c)
+{
+	char ch = 'C';
+
+	if(isupper (ch))
+	{
+		
+		return (1);
+	}
+	else 
+			return (0);
+}
+

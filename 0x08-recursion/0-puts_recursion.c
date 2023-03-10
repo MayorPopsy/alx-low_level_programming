f1ab457235fadf909cb6d5634a31bcb7020e2c61
Mayowa <mayowapopoola110@gmail.com>
2023-03-10 01:31:27 -0800

    .

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - A function that prints string followed by a new line
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion("\n puts with recursion");
+	return (0);
+}

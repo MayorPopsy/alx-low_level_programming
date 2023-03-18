b82446020ac64292077471c7d7fdca81d80ab067
Mayowa <mayowapopoola110@gmail.com>
2023-03-18 08:16:43 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - A program that prints its name 
+ *@argc: Arguement count 
+ *@argv: Arguement variable
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}

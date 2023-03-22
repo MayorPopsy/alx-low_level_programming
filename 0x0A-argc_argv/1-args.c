5717179d02ff1812046582d42b66a937304624e9
Mayowa <mayowapopoola110@gmail.com>
2023-03-22 04:54:55 -0700

    .

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,18 +2,16 @@
 #include "main.h"
 
 /**
- * main - Program that prints number of arguement 
- * @argc - number of arguement count 
- * @argv - number of variables
- * Return: 0 success 
+ * main - Program that prints number of arguement
+ * @argc: number of arguement count
+ * @argv: number of variables
+ * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	printf("%d\n",argc);
-	for(i=1;i<argc;i++)
-	{
-		printf("%s",argv[i]);
-	}
-	return 0;
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
